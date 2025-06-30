c6aa2edadaee3e06b04b45cfae3007717eefd027
Deesh Singla <deeshsingla@gmail.com>
2025-06-30 10:44:32 +0530

    added cors middleware

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import jwt from "jsonwebtoken"
+import cors from "cors";
 import { signupRouter } from "./routers/signupRouter.js";
 import { loginRouter } from "./routers/loginRouter.js";
 import { meRouter } from "./routers/meRouter.js";
 import { todoRouter } from "./routers/todoRouter.js";
 export const JWT_SECRET = "DEESH'S TODO"
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 export let users = [];
@@ -34,4 +36,8 @@ app.use("/signup", signupRouter)
 app.use("/login", loginRouter)
 app.use("/me", meRouter);
 app.use("/todo", auth, todoRouter)
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
+});
 app.listen(3000);
